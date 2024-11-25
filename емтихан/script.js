e814f6fc6c99f3ce9cc65c973feda2dd93827060
Islam <islam20110811p@gmail.com>
2024-11-25 16:45:30 +0500

    емтихан

---
+++ емтихан/script.js
@@ -0,0 +1,62 @@
+// function qwerty() {
+//     var num1 = +prompt("Кез келген бір санды енгізіңіз:");
+//     var num2 = +prompt("Тағы бір санды енгізіңіз:");
+//     var tanba = prompt("Кез келген таңбаны енгізіңіз ");
+//     var a = 0;
+
+//     if (tanba == '+') {
+//         a = num1 + num2;
+//     } else if (tanba == '-') {
+//         a = num1 - num2;
+//     } else if (tanba == '*') {
+//         a = num1 * num2;
+//     } else if (tanba == '/') {
+//     return num1 , num2 ;
+// }
+// }
+
+// alert(qwerty());
+
+
+
+// function qwerty() {
+//     var num1 = 98; 
+//     alert("1 - 100 дейін аралығында санды жазыңыз");
+
+//     while (true) {
+//         var num2 = +prompt("Болжам санды жазыңыз");
+
+//         if (num2 === num1) {
+//             alert("Сіз жасырылған санды таптыңыз!");
+//             break;
+//         } else if (num2 < num1) {
+//             alert("Сан жоғары!");
+//         } else if (num2 > num1) {
+//             alert("Сан төмен!");
+//         }
+//         return num1 , num2 ;
+//     }
+// }
+
+// qwerty(); 
+
+
+// function supermarket (num1 , num2 , num3  ){
+//      var num1 = +prompt("Затын бағасы");
+//      var num2 = prompt("Қанша зат сатып алдыныз");
+//      var num3 = 0; 
+
+//      for (var i = 1 ; i < Infinity ; i ++){
+//         if (num1 > "5000"){
+//             num3 = num1 * 100  && num1 % 10;
+//         }else if (num1 > "10000"){
+//             num3= num1 * 100 && num1 % 20;
+//         }else if (num1 >  "20000"){
+//             num3 = num1 * 100 && num1 % 30;
+//         }
+//        num3-=num1;
+//         return num1 , num2 , num3;
+//      }
+// }
+
+// alert(supermarket());
